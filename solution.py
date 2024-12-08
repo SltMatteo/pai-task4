6a2c3fb9a1bc2109e6f730d1cbbce4cf621e597f
SltMatteo <74387871+SltMatteo@users.noreply.github.com>
2024-12-08 18:57:50 +0100

    add print

--- solution.py
+++ solution.py
@@ -269,6 +269,8 @@ if __name__ == '__main__':
         # seeding to ensure determinism
         seed = int(seed)
         print("\n Running seed: " + str(seed) + "\n") #Matteo
+        print(" currently running: decaying noise \n")
+
         for fn in [random.seed, np.random.seed, torch.manual_seed]:
             fn(seed)
         torch.backends.cudnn.deterministic = True
