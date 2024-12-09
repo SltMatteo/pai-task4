bf41a5bf547396903450db310bc7d7cc79696279
SltMatteo <74387871+SltMatteo@users.noreply.github.com>
2024-12-09 03:56:50 +0100

    add bash log

--- runner.sh
+++ runner.sh
@@ -1,2 +1,4 @@
 docker build --tag task4 .
 docker run --init --rm -u $(id -u):$(id -g) -v "$( cd "$( dirname "$0" )" && pwd )":/results task4
+
+echo "this was the 'main' run [2 layers, 256 units]."
